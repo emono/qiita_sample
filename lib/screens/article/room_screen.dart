4fa0a6e2a8577712404e36de01b760bebfad5ffc
emono <ababababa0222@gmail.com>
2021-03-14 09:44:42 +0900

    いったん不要なメソッドをコメントアウトした

--- lib/screens/article/room_screen.dart
+++ lib/screens/article/room_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:qiita_sample/data/entities/qiita_info.dart';
-import 'package:qiita_sample/screens/article/article_item.dart';
-import 'package:qiita_sample/screens/article/article_repository.dart';
-import 'package:qiita_sample/screens/article/article_state_notifier.dart';
-import 'package:qiita_sample/screens/article_detail/article_detail_screen.dart';
+// import 'package:qiita_sample/data/entities/room_entity.dart';
+import 'package:qiita_sample/screens/article/room_item.dart';
+import 'package:qiita_sample/screens/article/room_repository.dart';
+import 'package:qiita_sample/screens/article/room_state_notifier.dart';
+// import 'package:qiita_sample/screens/article_detail/article_detail_screen.dart';
 
 class RoomScreen extends StatelessWidget {
   @override
@@ -23,8 +23,8 @@ class RoomScreen extends StatelessWidget {
 
 class _List extends StatelessWidget {
   final articleStateNotifier = StateNotifierProvider(
-    (_) => ArticleStateNotifier(
-      ArticleRepository(),
+    (_) => RoomStateNotifier(
+      RoomRepository(),
     ),
   );
 
@@ -34,29 +34,29 @@ class _List extends StatelessWidget {
       builder: (context, watch, child) {
         final state = watch(articleStateNotifier.state);
         return ListView.builder(
-          itemCount: state.articles.length,
-          itemBuilder: (context, int position) => ArticleItem(
-            qiitaInfo: state.articles[position],
-            onArticleClicked: (qiitaInfo) => _openArticleWebPage(
-              context,
-              qiitaInfo,
-            ),
+          itemCount: state.rooms.length,
+          itemBuilder: (context, int position) => RoomItem(
+            room: state.rooms[position],
+            // onArticleClicked: (qiitaInfo) => _openArticleWebPage(
+            //   context,
+            //   qiitaInfo,
+            // ),
           ),
         );
       },
     );
   }
 
-  void _openArticleWebPage(
-    BuildContext context,
-    QiitaInfo qiitaInfo,
-  ) {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (context) => ArticleDetailScreen(
-          qiitaInfo: qiitaInfo,
-        ),
-      ),
-    );
-  }
+  // void _openArticleWebPage(
+  //   BuildContext context,
+  //   QiitaInfo qiitaInfo,
+  // ) {
+  //   Navigator.of(context).push(
+  //     MaterialPageRoute(
+  //       builder: (context) => ArticleDetailScreen(
+  //         qiitaInfo: qiitaInfo,
+  //       ),
+  //     ),
+  //   );
+  // }
 }
