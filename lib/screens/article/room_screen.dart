38840c22e117a6ce44a4543717353205ad3bc9aa
emono <ababababa0222@gmail.com>
2021-03-14 09:30:12 +0900

    room一覧の画面を追加

---
+++ lib/screens/article/room_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:qiita_sample/data/entities/qiita_info.dart';
+import 'package:qiita_sample/screens/article/article_item.dart';
+import 'package:qiita_sample/screens/article/article_repository.dart';
+import 'package:qiita_sample/screens/article/article_state_notifier.dart';
+import 'package:qiita_sample/screens/article_detail/article_detail_screen.dart';
+
+class RoomScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'cast',
+        ),
+        centerTitle: true,
+      ),
+      body: _List(),
+    );
+  }
+}
+
+class _List extends StatelessWidget {
+  final articleStateNotifier = StateNotifierProvider(
+    (_) => ArticleStateNotifier(
+      ArticleRepository(),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, watch, child) {
+        final state = watch(articleStateNotifier.state);
+        return ListView.builder(
+          itemCount: state.articles.length,
+          itemBuilder: (context, int position) => ArticleItem(
+            qiitaInfo: state.articles[position],
+            onArticleClicked: (qiitaInfo) => _openArticleWebPage(
+              context,
+              qiitaInfo,
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _openArticleWebPage(
+    BuildContext context,
+    QiitaInfo qiitaInfo,
+  ) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => ArticleDetailScreen(
+          qiitaInfo: qiitaInfo,
+        ),
+      ),
+    );
+  }
+}
